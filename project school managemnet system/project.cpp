e569b23bc96dc74014e263c8f78c41f2e7e7c915
Alitaimoor-10 <lapard666@gmail.com>
2025-12-10 10:57:30 +0500

    Add files via upload

---
+++ project school managemnet system/project.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "School Management System" << endl;
+    cout << "1. Student Module" << endl;
+    cout << "2. Teacher Module" << endl;
+    cout << "3. Attendance" << endl;
+    cout << "4. Fees" << endl;
+    cout << "5. Exit" << endl;
+}
