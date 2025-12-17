fbdac4d8a9fb795e42cc544abe188de5e62c4f18
“[firstname <ithadali45@gmail.com>
2025-12-17 18:36:10 +0500

    Added timetable data structure (2D array)

---
+++ timetable.h
@@ -0,0 +1,13 @@
+#ifndef TIMETABLE_H
+#define TIMETABLE_H
+
+#include <string>
+using namespace std;
+
+const int DAYS = 5;
+const int LECTURES = 4;
+
+extern string days[DAYS];
+extern string timetable[DAYS][LECTURES];
+
+#endif
