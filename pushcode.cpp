05875bc5a51a195d16b0f38068028e782a592f27
“[firstname <ithadali45@gmail.com>
2025-12-20 09:40:54 +0500

    Initial commit

---
+++ pushcode.cpp
@@ -0,0 +1,17 @@
+#include "timetable.h"
+
+string days[DAYS] = {
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday"
+};
+
+string timetable[DAYS][LECTURES] = {
+    {"", "", "", ""},
+    {"", "", "", ""},
+    {"", "", "", ""},
+    {"", "", "", ""},
+    {"", "", "", ""}
+};
